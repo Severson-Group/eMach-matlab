b7cc6380b4b3b136d6f2c37e2e989b7848047c98
Nishanth <nishanth@wisc.edu>
2019-02-20 08:50:26 -0600

    Implement proposed precedence of rotation over translation

--- fea/magnet/model/mn_drawHollowRect.m
+++ fea/magnet/model/mn_drawHollowRect.m
@@ -21,7 +21,7 @@ validateattributes(compHollowRectObj,{'compHollowRect'},{'nonempty'})
 shift_xy = compHollowRectObj.location.anchor_xyz(1:2);
 theta = compHollowRectObj.location.rotate_xyz(3).toRadians;
 
-axis = [0,0] + shift_xy(1:2); 
+axis = [0,0];
 l_o=compHollowRectObj.dim_l_o;
 b_o=compHollowRectObj.dim_b_o;
 t1=compHollowRectObj.dim_t1;
@@ -29,24 +29,35 @@ t2=compHollowRectObj.dim_t2;
 t3=compHollowRectObj.dim_t3;
 t4=compHollowRectObj.dim_t4;
 
+%%Create inner and outer points
 points_i=[axis(1)+t1,axis(2)+t4; axis(1)+t1,axis(2)+b_o-t2;...
           l_o-t3+axis(1), b_o-t2+axis(2);l_o-t3+axis(1),t4+axis(2);];
 points_o = [axis(1),axis(2); axis(1),axis(2)+b_o; axis(1)+l_o, ....
             axis(2)+b_o; axis(1)+l_o,axis(2)];
-rotate=[cos(theta), -sin(theta); sin(theta), cos(theta)];
+        
+%%Rotation Matrix
+rotate=[cos(theta), -sin(theta);...
+        sin(theta), cos(theta)];
 
+%%Rotate
 for j=1:4
 points_i(j,:)=rotate*points_i(j,:)'
 points_o(j,:)=rotate*points_o(j,:)'
 end
 
-%% Inner Rectangle
+%Shift Points
+points_i(:,1)= points_i(:,1)+ shift_xy(1);
+points_i(:,2)=points_i(:,2)+ shift_xy(2);
+points_o(:,1)= points_o(:,1)+ shift_xy(1);
+points_o(:,2)=points_o(:,2)+ shift_xy(2);
+
+%% Draw Inner Rectangle
 [l_i1] = mn_dv_newline(mn, points_i(1,:),points_i(2,:));
 [l_i2] = mn_dv_newline(mn, points_i(2,:),points_i(3,:));
 [l_i3] = mn_dv_newline(mn, points_i(3,:),points_i(4,:));
 [l_i4] = mn_dv_newline(mn, points_i(4,:),points_i(1,:));
 
-%% Outer Rectangle
+%% Draw Outer Rectangle
 [l_o1] = mn_dv_newline(mn, points_o(1,:),points_o(2,:));
 [l_o2] = mn_dv_newline(mn, points_o(2,:),points_o(3,:));
 [l_o3] = mn_dv_newline(mn, points_o(3,:),points_o(4,:));
