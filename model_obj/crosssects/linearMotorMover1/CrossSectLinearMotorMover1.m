347ef9a522c89eccb991b6b29a0e1064409966e5
Anvar <khamitov@wisc.edu>
2019-02-25 14:26:08 -0600

    Update CrossSectLinearMotorMover1.m and CrossSectLinearMotorStator.m to add indentation

--- model_obj/crosssects/linearMotorMover1/CrossSectLinearMotorMover1.m
+++ model_obj/crosssects/linearMotorMover1/CrossSectLinearMotorMover1.m
@@ -23,59 +23,55 @@ classdef CrossSectLinearMotorMover1 < CrossSectBase
         end
                 
         function draw(obj, drawer)
-            validateattributes(drawer, {'Drawer2dBase'}, {'nonempty'});
-        
-        r_si = obj.dim_r_si;
-        w_r = obj.dim_w_r;
-        w_ra = obj.dim_w_ra;
-        w_rr = obj.dim_w_rr;
-        r_ri = obj.dim_r_ri;
-        d_rm = obj.dim_d_rm;
-        g = obj.dim_g;
-        Q = obj.dim_Q;   
-        
-        d_ri = r_si - g - r_ri - d_rm;
-        w_rs = (w_r - w_ra - 2*w_rr)/2;
-        
-        x1 = r_ri;
-        x2 = r_ri + d_ri;
-        x3 = r_ri + d_ri + d_rm;
-        
-        y1 = 0;
-        y2 = w_rs;
-        y3 = w_rs + w_rr;
-        y4 = w_rs + w_rr + w_ra;
-        y5 = w_r - w_rs;
-        y6 = w_r;
+            validateattributes(drawer, {'Drawer2dBase'}, {'nonempty'});   
+            r_si = obj.dim_r_si;
+            w_r = obj.dim_w_r;
+            w_ra = obj.dim_w_ra;
+            w_rr = obj.dim_w_rr;
+            r_ri = obj.dim_r_ri;
+            d_rm = obj.dim_d_rm;
+            g = obj.dim_g;
+            Q = obj.dim_Q;   
+            d_ri = r_si - g - r_ri - d_rm;
+            w_rs = (w_r - w_ra - 2*w_rr)/2;  
             
-        x = [ x1, x2, x2, x1, x2, x3, x2, x3, x2, x3, x2, x3];
-        y = [ y1, y1, y6, y6, y2, y2, y3, y3, y4, y4, y5, y5];
-        
-        p1 = [x(1), y(1)];
-        p2 = [x(2), y(2)];
-        p3 = [x(3), y(3)];
-        p4 = [x(4), y(4)];
-        p5 = [x(5), y(5)];
-        p6 = [x(6), y(6)];
-        p7 = [x(7), y(7)];
-        p8 = [x(8), y(8)];
-        p9 = [x(9), y(9)];
-        p10 = [x(10), y(10)];
-        p11 = [x(11), y(11)];
-        p12 = [x(12), y(12)];
-        
-        [seg1] = drawer.drawLine(p1, p2);
-        [seg2] = drawer.drawLine(p2, p3);
-        [seg3] = drawer.drawLine(p3, p4);
-        [seg4] = drawer.drawLine(p4, p1);        
-        [seg5] = drawer.drawLine(p5, p6);
-        [seg6] = drawer.drawLine(p7, p8);
-        [seg7] = drawer.drawLine(p9, p10);
-        [seg8] = drawer.drawLine(p11, p12);
-        [seg9] = drawer.drawLine(p6, p8);
-        [seg10] = drawer.drawLine(p8, p10);
-        [seg11] = drawer.drawLine(p10, p12);
-      
+            x1 = r_ri;
+            x2 = r_ri + d_ri;
+            x3 = r_ri + d_ri + d_rm;
+            
+            y1 = 0;
+            y2 = w_rs;
+            y3 = w_rs + w_rr;
+            y4 = w_rs + w_rr + w_ra;
+            y5 = w_r - w_rs;
+            y6 = w_r;
+            
+            x = [ x1, x2, x2, x1, x2, x3, x2, x3, x2, x3, x2, x3];
+            y = [ y1, y1, y6, y6, y2, y2, y3, y3, y4, y4, y5, y5];      
+            p1 = [x(1), y(1)];
+            p2 = [x(2), y(2)];
+            p3 = [x(3), y(3)];
+            p4 = [x(4), y(4)];
+            p5 = [x(5), y(5)];
+            p6 = [x(6), y(6)];
+            p7 = [x(7), y(7)];
+            p8 = [x(8), y(8)];
+            p9 = [x(9), y(9)];
+            p10 = [x(10), y(10)];
+            p11 = [x(11), y(11)];
+            p12 = [x(12), y(12)];
+            [seg1] = drawer.drawLine(p1, p2);
+            [seg2] = drawer.drawLine(p2, p3);
+            [seg3] = drawer.drawLine(p3, p4);
+            [seg4] = drawer.drawLine(p4, p1);        
+            [seg5] = drawer.drawLine(p5, p6);
+            [seg6] = drawer.drawLine(p7, p8);
+            [seg7] = drawer.drawLine(p9, p10);
+            [seg8] = drawer.drawLine(p11, p12);
+            [seg9] = drawer.drawLine(p6, p8);
+            [seg10] = drawer.drawLine(p8, p10);
+            [seg11] = drawer.drawLine(p10, p12);
+            
         end
         
         function select(obj)
--- model_obj/crosssects/linearMotorStator/CrossSectLinearMotorStator.m
+++ model_obj/crosssects/linearMotorStator/CrossSectLinearMotorStator.m
@@ -28,83 +28,80 @@ classdef CrossSectLinearMotorStator < CrossSectBase
                 
         function draw(obj, drawer)
             validateattributes(drawer, {'Drawer2dBase'}, {'nonempty'});
+            w_s = obj.dim_w_s;
+            w_st = obj.dim_w_st;
+            w_so = obj.dim_w_so;
+            r_so = obj.dim_r_so;
+            r_si = obj.dim_r_si;
+            d_so = obj.dim_d_so;
+            d_sp = obj.dim_d_sp;
+            d_sy = obj.dim_d_sy;
+            r_st = obj.dim_r_st;       
+            r_sf = obj.dim_r_sf;
+            r_sb = obj.dim_r_sb;
+            Q = obj.dim_Q;  
             
-        w_s = obj.dim_w_s;
-        w_st = obj.dim_w_st;
-        w_so = obj.dim_w_so;
-        r_so = obj.dim_r_so;
-        r_si = obj.dim_r_si;
-        d_so = obj.dim_d_so;
-        d_sp = obj.dim_d_sp;
-        d_sy = obj.dim_d_sy;
-        r_st = obj.dim_r_st;
-        r_sf = obj.dim_r_sf;
-        r_sb = obj.dim_r_sb;
-        Q = obj.dim_Q;   
-        
-        x1 = r_si;
-        x2 = r_si + d_so;
-        x3 = r_si + d_sp;
-        x4 = r_so - d_sy;
-        x5 = r_so;
-        
-        y1 = 0;
-        y2 = w_st/2;
-        y3 = (w_s-2*w_so)/4;
-        y4 = y3+w_so;
-        y5 = y4+y3-y2;
-        y6 = y5+w_st;
-        y7 = y6+y3-y2;
-        y8 = y7+w_so;
-        y9 = y8+y3-y2;
-        y10 = y9+w_st/2;
+            x1 = r_si;
+            x2 = r_si + d_so;
+            x3 = r_si + d_sp;
+            x4 = r_so - d_sy;
+            x5 = r_so;
             
-        x = [ x1, x5, x5,  x1,  x1, x2, x3, x4, x4, x3, x2, x1...
-              x1, x2, x3, x4, x4, x3, x2, x1];
-        y = [ y1, y1, y10, y10, y8, y8, y9, y9, y6, y6, y7, y7...
-              y4, y4, y5, y5, y2, y2, y3, y3];
+            y1 = 0;
+            y2 = w_st/2;
+            y3 = (w_s-2*w_so)/4;
+            y4 = y3+w_so;
+            y5 = y4+y3-y2;
+            y6 = y5+w_st;
+            y7 = y6+y3-y2;
+            y8 = y7+w_so;
+            y9 = y8+y3-y2;
+            y10 = y9+w_st/2;
+            x = [ x1, x5, x5,  x1,  x1, x2, x3, x4, x4, x3, x2, x1...
+                  x1, x2, x3, x4, x4, x3, x2, x1];
+            y = [ y1, y1, y10, y10, y8, y8, y9, y9, y6, y6, y7, y7...
+                  y4, y4, y5, y5, y2, y2, y3, y3];
+            p1 = [x(1), y(1)];
+            p2 = [x(2), y(2)];
+            p3 = [x(3), y(3)];
+            p4 = [x(4), y(4)];
+            p5 = [x(5), y(5)];
+            p6 = [x(6), y(6)];
+            p7 = [x(7), y(7)];
+            p8 = [x(8), y(8)];
+            p9 = [x(9), y(9)];
+            p10 = [x(10), y(10)];
+            p11 = [x(11), y(11)];
+            p12 = [x(12), y(12)];
+            p13 = [x(13), y(13)];
+            p14 = [x(14), y(14)];
+            p15 = [x(15), y(15)];
+            p16 = [x(16), y(16)];
+            p17 = [x(17), y(17)];
+            p18 = [x(18), y(18)];
+            p19 = [x(19), y(19)];
+            p20 = [x(20), y(20)];
         
-        p1 = [x(1), y(1)];
-        p2 = [x(2), y(2)];
-        p3 = [x(3), y(3)];
-        p4 = [x(4), y(4)];
-        p5 = [x(5), y(5)];
-        p6 = [x(6), y(6)];
-        p7 = [x(7), y(7)];
-        p8 = [x(8), y(8)];
-        p9 = [x(9), y(9)];
-        p10 = [x(10), y(10)];
-        p11 = [x(11), y(11)];
-        p12 = [x(12), y(12)];
-        p13 = [x(13), y(13)];
-        p14 = [x(14), y(14)];
-        p15 = [x(15), y(15)];
-        p16 = [x(16), y(16)];
-        p17 = [x(17), y(17)];
-        p18 = [x(18), y(18)];
-        p19 = [x(19), y(19)];
-        p20 = [x(20), y(20)];
-        
-        [seg1] = drawer.drawLine(p1, p2);
-        [seg2] = drawer.drawLine(p2, p3);
-        [seg3] = drawer.drawLine(p3, p4);
-        [seg4] = drawer.drawLine(p4, p5);
-        [seg5] = drawer.drawLine(p5, p6);
-        [seg6] = drawer.drawLine(p6, p7);
-        [seg7] = drawer.drawLine(p7, p8);
-        [seg8] = drawer.drawLine(p8, p9);
-        [seg9] = drawer.drawLine(p9, p10);
-        [seg10] = drawer.drawLine(p10, p11);
-        [seg11] = drawer.drawLine(p11, p12);
-        [seg12] = drawer.drawLine(p12, p13);
-        [seg13] = drawer.drawLine(p13, p14);
-        [seg14] = drawer.drawLine(p14, p15);
-        [seg15] = drawer.drawLine(p15, p16);
-        [seg16] = drawer.drawLine(p16, p17);
-        [seg17] = drawer.drawLine(p17, p18);
-        [seg18] = drawer.drawLine(p18, p19);
-        [seg19] = drawer.drawLine(p19, p20);
-        [seg20] = drawer.drawLine(p20, p1);
+            [seg1] = drawer.drawLine(p1, p2);
+            [seg2] = drawer.drawLine(p2, p3);
+            [seg3] = drawer.drawLine(p3, p4);
+            [seg4] = drawer.drawLine(p4, p5);
+            [seg5] = drawer.drawLine(p5, p6);
+            [seg6] = drawer.drawLine(p6, p7);
+            [seg7] = drawer.drawLine(p7, p8);
+            [seg8] = drawer.drawLine(p8, p9);
+            [seg9] = drawer.drawLine(p9, p10);
+            [seg10] = drawer.drawLine(p10, p11);
+            [seg11] = drawer.drawLine(p11, p12);
+            [seg12] = drawer.drawLine(p12, p13);
+            [seg13] = drawer.drawLine(p13, p14);
+            [seg14] = drawer.drawLine(p14, p15);
+            [seg15] = drawer.drawLine(p15, p16);
+            [seg16] = drawer.drawLine(p16, p17);
+            [seg17] = drawer.drawLine(p17, p18);
+            [seg18] = drawer.drawLine(p18, p19);
+            [seg19] = drawer.drawLine(p19, p20);
+            [seg20] = drawer.drawLine(p20, p1);
       
         end
         
