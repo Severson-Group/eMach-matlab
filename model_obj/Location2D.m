c6d8236a3fc1b8ed86d81db717a455893d3c0d10
Nicholas Hemenway <nhemenway@wisc.edu>
2019-02-27 22:57:19 -0600

    add relative function to Location2D class

--- model_obj/Location2D.m
+++ model_obj/Location2D.m
@@ -21,7 +21,7 @@ classdef Location2D
                       sin(theta),  cos(theta) ];
         end
         
-        function rotated_coords = transformCoords(obj, coords, add_theta)
+        function rotatedCoords = transformCoords(obj, coords, addTheta)
             
             %This function takes in an nx2 array of coordinates of the form
             %[x,y] and returns rotated and translated coordinates. The
@@ -30,16 +30,40 @@ classdef Location2D
             %additional angle of "add_theta" to the obj.theta attribute.
             
             if exist('add_theta','var')
-                validateattributes(add_theta, {'DimAngular'}, {'size',[1,1]})
-                add_theta = add_theta.toRadians() + obj.theta.toRadians();
+                validateattributes(addTheta, {'DimAngular'}, {'size',[1,1]})
+                add_theta = addTheta.toRadians() + obj.theta.toRadians();
                 T = [ cos(add_theta), -sin(add_theta); ...
                       sin(add_theta),  cos(add_theta) ];
             else
                 T = obj.R;
             end
                
-            rotated_coords = transpose(T*coords');
+            rotatedCoords = transpose(T*coords');
         end
+        
+        function locObj = relative(obj, relLinear, relAngular)
+            
+
+            validateattributes(relLinear, {'DimLinear'}, {'size',[1,2]})
+            validateattributes(relAngular, {'DimAngular'}, {'size',[1,1]})
+            
+            anchor = obj.anchor_xy + relLinear;
+            angle = obj.theta + relAngular;
+            
+            %NOTE, once operator overloading is created, there will be no
+            %need to create dimension objects over again. The above code is
+            %assuming the two quantities being added are of the same units
+            %and the code below assumes what the dimensions were which is
+            %clearly wrong
+            
+            anchor = DimMillimeter(anchor);
+            angle = DimDegree(angle);
+            
+            locObj = Location2D('anchor_xy', anchor, ...
+                                'theta', angle );
+      
+        end
+            
         
     end
     
