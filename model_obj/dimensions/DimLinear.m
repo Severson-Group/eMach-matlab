a79b393120bf8daf38df1fc24694082ef004ea86
Nathan Petersen <npetersen2@wisc.edu>
2019-03-18 21:39:29 -0500

    Update linear dim for added operator overloading

--- model_obj/dimensions/DimLinear.m
+++ model_obj/dimensions/DimLinear.m
@@ -17,6 +17,41 @@ classdef DimLinear < DimBase
             % Return object of same type as LHS
             r = feval(class(lhs), DimMillimeter(sum));
         end
+        
+        function r = minus(lhs, rhs)
+            validateattributes(lhs, {'DimLinear'}, {'nonempty'});
+            validateattributes(rhs, {'DimLinear'}, {'nonempty'});
+
+            % Convert to DimMillimeter for calculation
+            sum = double(lhs.toMillimeter()) - double(rhs.toMillimeter());
+
+            % Return object of same type as LHS
+            r = feval(class(lhs), DimMillimeter(sum));
+        end
+        
+        function r = uminus(obj)
+            validateattributes(obj, {'DimLinear'}, {'nonempty'});
+
+            % Convert to DimMillimeter for calculation
+            val = -double(obj.toMillimeter());
+
+            % Return object of same type as obj
+            r = feval(class(obj), DimMillimeter(val));
+        end
+        
+        function r = uplus(obj)
+            validateattributes(obj, {'DimLinear'}, {'nonempty'});
+
+            % Convert to DimMillimeter for calculation
+            val = double(obj.toMillimeter());
+            
+            if (val < 0)
+                val = val * -1;
+            end
+
+            % Return object of same type as obj
+            r = feval(class(obj), DimMillimeter(val));
+        end
     end
     
     methods(Abstract = true)
