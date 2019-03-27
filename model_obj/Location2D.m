aa1a2bf4174b1cfae603dbe10a8cbdb9c37ee261
Nicholas Hemenway <nhemenway@wisc.edu>
2019-03-27 11:23:45 -0500

    replace location_2D with file on develop branch

--- model_obj/Location2D.m
+++ model_obj/Location2D.m
@@ -43,32 +43,7 @@ classdef Location2D
             rotateCoords = transpose(T*coords');
             transCoords(:,1) = rotateCoords(:,1) + obj.anchor_xy(1);
             transCoords(:,2) = rotateCoords(:,2) + obj.anchor_xy(2);
-            transCoords(:,2) = rotateCoords(:,2) + obj.anchor_xy(2);
         end
-        
-        function locObj = relative(obj, relLinear, relAngular)
-            
-
-            validateattributes(relLinear, {'DimLinear'}, {'size',[1,2]})
-            validateattributes(relAngular, {'DimAngular'}, {'size',[1,1]})
-            
-            anchor = obj.anchor_xy + relLinear;
-            angle = obj.theta + relAngular;
-            
-            %NOTE, once operator overloading is created, there will be no
-            %need to create dimension objects over again. The above code is
-            %assuming the two quantities being added are of the same units
-            %and the code below assumes what the dimensions were which is
-            %clearly wrong
-            
-            anchor = DimMillimeter(anchor);
-            angle = DimDegree(angle);
-            
-            locObj = Location2D('anchor_xy', anchor, ...
-                                'theta', angle );
-      
-        end
-            
         
     end
     
