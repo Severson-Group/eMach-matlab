67d073b0c6f9f2f3d0eeac8bcbb4a41d7afa3b1b
Nathan Petersen <npetersen2@wisc.edu>
2019-05-16 11:44:38 -0500

    Implement outer rotor. S>1 is not yet implemented

--- model_obj/crosssects/outerRotor/CrossSectOuterRotor.m
+++ model_obj/crosssects/outerRotor/CrossSectOuterRotor.m
@@ -6,18 +6,14 @@ classdef CrossSectOuterRotor < CrossSectBase
     
     
     properties (GetAccess = 'public', SetAccess = 'protected')
-        dim_alpha_st; % span angle of tooth: class type DimAngular
-        dim_alpha_so; % angle of tooth edge: class type DimAngular
-        dim_r_si;     % inner radius of stator teeth: class type DimLinear
-        dim_d_so;     % tooth edge length: class type DimLinear
-        dim_d_sp;     % tooth tip length: class type DimLinear
-        dim_d_st;     % tooth base length: class type DimLinear
-        dim_d_sy;     % back iron thickness: class type DimLinear
-        dim_w_st;     % tooth base width: class type DimLinear
-        dim_r_st;     % fillet on outer tooth: class type DimLinear
-        dim_r_sf;     % fillet between tooth tip and base: class type DimLinear
-        dim_r_sb;     % fillet at tooth base: class type DimLinear
-        dim_Q;        % number of stator slots (integer)
+        dim_alpha_rs; % span angle of magnet segment: class type DimAngular
+        dim_alpha_rm; % span angle of entire magnet: class type DimAngular
+        dim_R_ro;     % outer radius of rotor: class type DimLinear.
+        dim_d_rp;     % between pole depth: class type DimLinear
+        dim_d_ri;     % back iron depth: class type DimLinear
+        dim_d_rs;     % segment divider depth: class type DimLinear
+        dim_P;        % number of pole pairs (integer)
+        dim_S;        % number of segments / pole (integer)
         
     end
     
@@ -32,48 +28,43 @@ classdef CrossSectOuterRotor < CrossSectBase
             
             % create local variables of all of the attributes
             % for more readable code
-            alpha_st = obj.dim_alpha_st.toRadians();
-            alpha_so = obj.dim_alpha_so.toRadians();
-            r_si = obj.dim_r_si;
-            d_so = obj.dim_d_so;
-            d_sp = obj.dim_d_sp;
-            d_st = obj.dim_d_st;
-            d_sy = obj.dim_d_sy;
-            w_st = obj.dim_w_st;
-            r_st = obj.dim_r_st;
-            r_sf = obj.dim_r_sf;
-            r_sb = obj.dim_r_sb;
-            Q = obj.dim_Q;
-			
-			% TODO: this code is from Nick and draws the inner rotor stator...
-			% Please modify to draw the outer rotor stator as drawn in diagram
+            alpha_rs = obj.dim_alpha_rs.toRadians();
+            alpha_rm = obj.dim_alpha_rm.toRadians();
+            R_ro = obj.dim_R_ro;
+            d_rp = obj.dim_d_rp;
+            d_ri = obj.dim_d_ri;
+            d_rs = obj.dim_d_rs;
+            P = obj.dim_P;
+            S = obj.dim_S;
+                   
+            alpha_total = DimDegree(180/P).toRadians();
             
-            alpha_total = DimDegree(360/Q).toRadians();
+            % outer arc
+            r = R_ro;
+            x1 = r * cos(alpha_total / 2);
+            y1 = r * sin(alpha_total / 2);
             
-            x1 = r_si*cos(alpha_st/2);
-            beta2 = alpha_st/2 - alpha_so;
-            x2 = x1 + d_so*cos( beta2 );
-            r3 = r_si + d_sp;
-            beta3 = asin((w_st/2)/r3);
-            x3 = r3*cos(beta3);
-            r4 = r3 + d_st;
-            beta4 = asin((w_st/2)/r4);
-            x4 = r4*cos(beta4);
-            x5 = r4*cos(alpha_total/2);
-            x6 = (r4 + d_sy)*cos(alpha_total/2);
-            disp(r4 + d_sy)
+            % inner arc between poles
+            r = R_ro - d_ri - d_rp;
+            x2 = r * cos(alpha_rm / 2);
+            y2 = r * sin(alpha_rm / 2);
+            x3 = r * cos(alpha_total / 2);
+            y3 = r * sin(alpha_total / 2);
             
-            y1 = r_si*sin(alpha_st/2);
-            y2 = y1 + d_so*sin(beta2);
-            y3 = w_st/2;
-            y4 = w_st/2;
-            y5 = r4*sin(alpha_total/2);
-            y6 = (r4 + d_sy)*sin(alpha_total/2);
+            % line containing region between poles
+            r = R_ro - d_ri;
+            x4 = r * cos(alpha_rm / 2);
+            y4 = r * sin(alpha_rm / 2);
             
-            x_arr = [ x1, x1, x2, x3, x4, x5, x6, x6, x5, x4, x3, x2 ];
-            y_arr = [-y1, y1, y2, y3, y4, y5, y6, -y6, -y5, -y4, -y3, -y2];
+            x_arr = [x1,  x1, x2, x3,  x2,  x3, x4,  x4];
+            y_arr = [y1, -y1, y2, y3, -y2, -y3, y4, -y4];
+
+            % TODO: please add support for S > 1
+            if S > 1
+               error("S>1 not supported!"); 
+            end
             
-            for i = 1:Q
+            for i = 1 : (2*P)
                 p = obj.location.transformCoords([x_arr',y_arr'], DimRadian((i-1)*alpha_total));
                 
                 x = p(:,1);
@@ -81,33 +72,29 @@ classdef CrossSectOuterRotor < CrossSectBase
 
                 p1 = [x(1), y(1)];
                 p2 = [x(2), y(2)];
+                
                 p3 = [x(3), y(3)];
                 p4 = [x(4), y(4)];
+                
                 p5 = [x(5), y(5)];
                 p6 = [x(6), y(6)];
+                
                 p7 = [x(7), y(7)];
                 p8 = [x(8), y(8)];
-                p9 = [x(9), y(9)];
-                p10 = [x(10), y(10)];
-                p11 = [x(11), y(11)];
-                p12 = [x(12), y(12)];
 
-                arc1(i) = drawer.drawArc(obj.location.anchor_xy, p1, p2);
-                seg1(i) = drawer.drawLine(p2, p3);
-                seg2(i) = drawer.drawLine(p3, p4);
-                seg3(i) = drawer.drawLine(p4, p5);
-                arc2(i) = drawer.drawArc(obj.location.anchor_xy, p5, p6);
-                arc3(i) = drawer.drawArc(obj.location.anchor_xy, p8, p7);
-                arc4(i) = drawer.drawArc(obj.location.anchor_xy, p9, p10);
-                seg4(i) = drawer.drawLine(p10, p11);
-                seg5(i) = drawer.drawLine(p11, p12);
-                seg6(i) = drawer.drawLine(p12, p1);
-            
+                arc1(i) = drawer.drawArc(obj.location.anchor_xy, p2, p1);
+                arc2(i) = drawer.drawArc(obj.location.anchor_xy, p3, p4);
+                arc3(i) = drawer.drawArc(obj.location.anchor_xy, p6, p5);
+                
+                seg1(i) = drawer.drawLine(p3, p7);
+                seg2(i) = drawer.drawLine(p5, p8);
+                
+                arc4(i) = drawer.drawArc(obj.location.anchor_xy, p8, p7);
             end
             
-            rad = (x3 + x4)/2;
+            rad = R_ro - (d_ri / 2);
             innerCoord = obj.location.transformCoords([rad, 0]); 
-            segments = [arc1, seg1, seg2, seg3, arc2, arc3, arc4, seg4, seg5, seg6];
+            segments = [arc1];%[arc1, seg1, seg2, seg3, arc2, arc3, arc4, seg4, seg5, seg6];
             csToken = CrossSectToken(innerCoord, segments);
         end
         
@@ -121,18 +108,14 @@ classdef CrossSectOuterRotor < CrossSectBase
             validateProps@CrossSectBase(obj);   
             
             %2. Validate the new properties that have been added here
-            validateattributes(obj.dim_alpha_st,{'DimAngular'},{'nonnegative', 'nonempty'});
-            validateattributes(obj.dim_alpha_so,{'DimAngular'},{'nonempty'});
-            validateattributes(obj.dim_r_si,{'DimLinear'},{'nonnegative','nonempty'})  ;          
-            validateattributes(obj.dim_d_so,{'DimLinear'},{'nonnegative','nonempty'});
-            validateattributes(obj.dim_d_sp,{'DimLinear'},{'nonnegative','nonempty'});
-            validateattributes(obj.dim_d_st,{'DimLinear'},{'nonnegative','nonempty'});
-            validateattributes(obj.dim_d_sy,{'DimLinear'},{'nonnegative','nonempty'});
-            validateattributes(obj.dim_w_st,{'DimLinear'},{'nonnegative','nonempty'});
-            validateattributes(obj.dim_r_st,{'DimLinear'},{'nonnegative','nonempty'});
-            validateattributes(obj.dim_r_sf,{'DimLinear'},{'nonnegative','nonempty'});
-            validateattributes(obj.dim_r_sb,{'DimLinear'},{'nonnegative','nonempty'});
-            validateattributes(obj.dim_Q,{'double'},{'nonnegative','nonempty','integer'});
+            validateattributes(obj.dim_alpha_rs,{'DimAngular'},{'nonnegative', 'nonempty'});
+            validateattributes(obj.dim_alpha_rm,{'DimAngular'},{'nonnegative', 'nonempty'});
+            validateattributes(obj.dim_R_ro,{'DimLinear'},{'nonnegative','nonempty'})  ;          
+            validateattributes(obj.dim_d_rp,{'DimLinear'},{'nonnegative','nonempty'});
+            validateattributes(obj.dim_d_ri,{'DimLinear'},{'nonnegative','nonempty'});
+            validateattributes(obj.dim_d_rs,{'DimLinear'},{'nonnegative','nonempty'});
+            validateattributes(obj.dim_P,{'double'},{'nonnegative','nonempty','integer'});
+            validateattributes(obj.dim_S,{'double'},{'nonnegative','nonempty','integer'});
          end
                   
          function obj = createProps(obj, len, args)
