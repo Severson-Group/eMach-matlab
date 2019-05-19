5793dfcf8ec893f7cc2f677691e1891beca8db67
ngadiyar93 <nishanth@wisc.edu>
2019-05-19 18:17:02 -0500

    Add script to plot FEA result

---
+++ examples/exampleAFPM2D/AFPM2DPlots.m
@@ -0,0 +1,34 @@
+clear all;
+close all;
+clc;
+load('AFPMSolution.mat')
+
+figure(1)
+plot(solutiondata.time,-solutiondata.torque,'color','r','linewidth',2.0);
+ylim([0,13])
+xlabel('Time [ms]');
+ylabel('Torque [Nm]');
+title('Torque from 2D-Equivalent Model');
+
+figure(2)
+plot(solutiondata.time,-solutiondata.flux_A,'color','r','linewidth',2.0);
+hold on
+plot(solutiondata.time,-solutiondata.flux_B,'color','m','linewidth',2.0);
+plot(solutiondata.time,-solutiondata.flux_C,'color','b','linewidth',2.0);
+xlabel('Time [ms]');
+ylabel('Flux Linkage [Wb]');
+title('Flux Linkage from 2D-Equivalent Model');
+legend(['Phase A'],['Phase B'],['Phase C']);
+
+figure(3)
+plot(solutiondata.time,solutiondata.voltage_A,'color','r','linewidth',2.0);
+hold on
+plot(solutiondata.time,solutiondata.voltage_B,'color','m','linewidth',2.0);
+plot(solutiondata.time,solutiondata.voltage_C,'color','b','linewidth',2.0);
+xlabel('Time [ms]');
+ylabel('Back EMF [V]');
+title('Back EMF from 2D-Equivalent Model');
+legend(['Phase A'],['Phase B'],['Phase C']);
+ylim([-75,75]);
+
+
