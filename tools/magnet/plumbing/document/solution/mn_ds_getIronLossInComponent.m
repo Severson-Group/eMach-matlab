0d09ad822613ba42257e7dae2fe2ed47c28a2f4b
Eric Severson <eric.severson@wisc.edu>
2019-05-28 22:35:23 -0500

    Add new MagNet plumbing functions
    - mn_d_getparameter to read parameters from MagNet
    - mn_ds_getIronLossInComponent to read eddy current/hysteresis loss

---
+++ tools/magnet/plumbing/document/solution/mn_ds_getIronLossInComponent.m
@@ -0,0 +1,24 @@
+function [hystLoss, eddyCurLoss] = mn_ds_getIronLossInComponent(mn, ...
+                                componentName, pid)
+%   MN_DS_GETIRONLOSSINCOMPONENT Get the eddy current and hystersis loss
+%
+%   mn = pointer to the MagNet project
+%   pid = ID of solved problem (optional)
+%   Returns taOhmicLoss
+%
+%
+%   Example: 
+
+if (exist('solutionID') ~= 1)
+    pid = 1;
+end
+
+invoke(MN, 'processcommand', 'ReDim Losses(1)');
+invoke(mn, 'processcommand', ...
+    ['call getDocument().getSolution().getIronLossInComponent(' ...
+    num2str(pid) ', "' componentName '", Losses)']);
+
+invoke(mn, 'processcommand', 'Call setVariant(0, Losses, "MATLAB")');
+Losses = invoke(MN,  'getVariant', 0, 'MATLAB');
+hystLoss = Losses(1);
+eddyCurLoss = Losses(2);
