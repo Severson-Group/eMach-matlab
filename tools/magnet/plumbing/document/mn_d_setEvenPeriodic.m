84aa8e1e0300b5a41d65451f01b086bc6eaf5b82
ngadiyar93 <nishanth@wisc.edu>
2019-06-03 00:14:49 -0500

    Update the plumbing function to `setEvenPeriodic`

--- tools/magnet/plumbing/document/mn_d_setEvenPeriodic.m
+++ tools/magnet/plumbing/document/mn_d_setEvenPeriodic.m
@@ -1,56 +1,61 @@
-function mn_d_setEvenPeriodic(mn, name,scale_factor, rotation_angle, RotationAxis, MirrorNormal, ShiftVector, Center)
+function[status] = mn_d_setEvenPeriodic(mn, name, scaleFactor, rotationAngle, rotationAxis, mirrorNormal, shiftVector, center)
 %MN_D_SETEVENPERIODIC Makes the boundary condition even periodic
 %   mn_d_setEvenPeriodic(mn, name,scale_factor, rotation_angle, RotationAxis, MirrorNormal, ShiftVector, Center)
 %   name: name assigned to the boundary condition
-%   scale_factor: set the scale_factor value
-%   rotation_angle: set the angle of rotation in degrees
-%   RotationAxis: rotation axis about which rotation_angle is set
-%   MirrorNormal: normal vector to the mirror plane
-%   ShiftVector: coordinates of the shift vector in (mm)
-%   Center: reference point coordinates in (mm)
+%   scaleFactor: set the scale_factor value
+%   rotationAngle: set the angle of rotation in degrees
+%   rotationAxis: rotation axis about which rotation_angle is set
+%   mirrorNormal: normal vector to the mirror plane
+%   shiftVector: coordinates of the shift vector in (mm)
+%   center: reference point coordinates in (mm)
 %   This is a wrapper for Document::setEvenPeriodic
-if strcmp(ShiftVector,'Null')== 0
+
+ if ~isempty(shiftVector)
     shift = 'ShiftVector';
     invoke(mn, 'processcommand', 'REDIM ShiftVector(2)');
-    invoke(mn, 'processcommand', sprintf('ShiftVector(0)= %s', num2str(ShiftVector(1))));
-    invoke(mn, 'processcommand', sprintf('ShiftVector(1)= %s', num2str(ShiftVector(2))));
-    invoke(mn, 'processcommand', sprintf('ShiftVector(2)= %s', num2str(ShiftVector(3))));
+    invoke(mn, 'processcommand', sprintf('ShiftVector(0)= %s', num2str(shiftVector(1))));
+    invoke(mn, 'processcommand', sprintf('ShiftVector(1)= %s', num2str(shiftVector(2))));
+    invoke(mn, 'processcommand', sprintf('ShiftVector(2)= %s', num2str(shiftVector(3))));
 else
     shift = 'Null';
 end
 
-if strcmp(RotationAxis,'Null')== 0
+
+  if  ~isempty(rotationAxis)
     axis = 'RotationAxis';
     invoke(mn, 'processcommand', 'REDIM RotationAxis(2)');
-    invoke(mn, 'processcommand', sprintf('RotationAxis(0)= %s', num2str(RotationAxis(1))));
-    invoke(mn, 'processcommand', sprintf('RotationAxis(1)= %s', num2str(RotationAxis(2))));
-    invoke(mn, 'processcommand', sprintf('RotationAxis(2)= %s', num2str(RotationAxis(3))));
+    invoke(mn, 'processcommand', sprintf('RotationAxis(0)= %s', num2str(rotationAxis(1))));
+    invoke(mn, 'processcommand', sprintf('RotationAxis(1)= %s', num2str(rotationAxis(2))));
+    invoke(mn, 'processcommand', sprintf('RotationAxis(2)= %s', num2str(rotationAxis(3))));
 else
     axis = 'Null';
 end
 
-if strcmp(MirrorNormal,'Null')== 0
+ if  ~isempty(mirrorNormal)
     normal = 'MirrorNormal';
     invoke(mn, 'processcommand', 'REDIM MirrorNormal(2)');
-    invoke(mn, 'processcommand', sprintf('MirrorNormal(0)= %s', num2str(MirrorNormal(1))));
-    invoke(mn, 'processcommand', sprintf('MirrorNormal(1)= %s', num2str(MirrorNormal(2))));
-    invoke(mn, 'processcommand', sprintf('MirrorNormal(2)= %s', num2str(MirrorNormal(3))));
+    invoke(mn, 'processcommand', sprintf('MirrorNormal(0)= %s', num2str(mirrorNormal(1))));
+    invoke(mn, 'processcommand', sprintf('MirrorNormal(1)= %s', num2str(mirrorNormal(2))));
+    invoke(mn, 'processcommand', sprintf('MirrorNormal(2)= %s', num2str(mirrorNormal(3))));
 else
     normal = 'Null';
 end
 
-if strcmp(Center,'Null')== 0
+   if ~isempty(center)
     reference = 'Center';
     invoke(mn, 'processcommand', 'REDIM Center(2)');
-    invoke(mn, 'processcommand', sprintf('Center(0)= %s', num2str(Center(1))));
-    invoke(mn, 'processcommand', sprintf('Center(1)= %s', num2str(Center(2))));
-    invoke(mn, 'processcommand', sprintf('Center(2)= %s', num2str(Center(3))));
+    invoke(mn, 'processcommand', sprintf('Center(0)= %s', num2str(center(1))));
+    invoke(mn, 'processcommand', sprintf('Center(1)= %s', num2str(center(2))));
+    invoke(mn, 'processcommand', sprintf('Center(2)= %s', num2str(center(3))));
 else
     reference = 'Null';
 end
 
 arguments = sprintf('"%s",%s,%s,%s, %s, %s, %s',...
-    name,num2str(scale_factor),num2str(rotation_angle),axis,normal,shift,reference);
+    name,num2str(scaleFactor),num2str(rotationAngle),axis,normal,shift,reference);
 
-invoke(mn, 'processcommand', ['call getDocument().setEvenPeriodic(' arguments ')']);
+invoke(mn, 'processcommand', ['ret = getDocument().setEvenPeriodic(' arguments ')']);
+
+invoke(mn, 'processcommand', 'call setvariant(0, ret)');
+status = invoke(mn, 'getvariant', 0);
 end
