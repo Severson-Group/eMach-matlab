1a9bd6d42fb22b44d70108aef07a9fdfe0ac3f38
ngadiyar93 <nishanth@wisc.edu>
2019-06-02 23:35:14 -0500

    Update variable name to provide accurate description

--- tools/magnet/plumbing/document/mn_d_makeMotionComponent.m
+++ tools/magnet/plumbing/document/mn_d_makeMotionComponent.m
@@ -1,23 +1,23 @@
-function [motion] = mn_d_makeMotionComponent(mn, surfacesVolume)
+function [motion] = mn_d_makeMotionComponent(mn, componentName)
 %MN_D_MAKEMOTIONCOMPONENT Creates a motion component.
 %   mn_d_setMotionComponent(mn, problemID, surfacesVolume)
 %       
-%   surfacesVolume - Single string or a cell array of strings of the
+%   componentName - Single string or a cell array of strings of the
 %       geometry to be moved. 
 %
 %   Return value: a path to the motion component.
 %   This is a wrapper for Document::makeMotionComponent
-if iscell(surfacesVolume) 
+if iscell(componentName) 
     invoke(mn, 'processcommand', ...
-        sprintf('REDIM nmArray(%i)', length(surfacesVolume)-1));
-    for i = 1:length(surfacesVolume)
+        sprintf('REDIM nmArray(%i)', length(componentName)-1));
+    for i = 1:length(componentName)
         invoke(mn, 'processcommand', ...
-            sprintf('nmArray(%i)= "%s"', i-1, surfacesVolume{i}));
+            sprintf('nmArray(%i)= "%s"', i-1, componentName{i}));
     end
 else
     invoke(mn, 'processcommand', 'REDIM nmArray(0)');
     invoke(mn, 'processcommand', ...
-            sprintf('nmArray(0)= "%s"', surfacesVolume));    
+            sprintf('nmArray(0)= "%s"', componentName));    
 end
 
 invoke(mn, 'processcommand', 'ret = getDocument.makeMotionComponent(nmArray)');
