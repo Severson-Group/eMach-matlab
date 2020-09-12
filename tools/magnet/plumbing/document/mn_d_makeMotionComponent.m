9d3315066df609440cbd203a3cb7c11a97281ec8
ngadiyar93 <nishanth@wisc.edu>
2020-09-12 16:57:12 -0500

    Update comments

--- tools/magnet/plumbing/document/mn_d_makeMotionComponent.m
+++ tools/magnet/plumbing/document/mn_d_makeMotionComponent.m
@@ -1,12 +1,13 @@
 function [motion] = mn_d_makeMotionComponent(mn, componentName)
 %MN_D_MAKEMOTIONCOMPONENT Creates a motion component.
-%   mn_d_setMotionComponent(mn, problemID, surfacesVolume)
+%   mn_d_setMotionComponent(mn, componentName)
 %       
 %   componentName - Single string or a cell array of strings of the
 %       geometry to be moved. 
 %
-%   Return value: a path to the motion component.
+%   Return value - a path to the motion component.
 %   This is a wrapper for Document::makeMotionComponent
+
 if iscell(componentName) 
     invoke(mn, 'processcommand', ...
         sprintf('REDIM nmArray(%i)', length(componentName)-1));
