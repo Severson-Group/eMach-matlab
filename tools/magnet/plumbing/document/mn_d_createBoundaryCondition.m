63425c8e8e1ab722fbb529a86f97ccb831279701
ngadiyar93 <nishanth@wisc.edu>
2020-09-12 16:54:14 -0500

    Update Comments and specify the return value

--- tools/magnet/plumbing/document/mn_d_createBoundaryCondition.m
+++ tools/magnet/plumbing/document/mn_d_createBoundaryCondition.m
@@ -1,13 +1,19 @@
 function[bc_path]= mn_d_createBoundaryCondition(mn, pathArray, name)
 %MN_D_CREATEBOUNDARYCONDITION Creates a boundary condition.
-%   mn_d_createBoundaryCondition(mn, surfacesFace, Face , name )
+%   mn_d_createBoundaryCondition(mn, pathArray, name)
+%
 %   pathArray - Single string or a cell array of strings of the
 %       geometry to which boundary condition is assigned. This will be in
 %       the format {compName,Face#i} where compName is the name of the
 %       component and Face#i is the face number to which the boundary
 %       condition is applied. i is an integer denoting face number.
-%   name: name assigned to the boundary condition
+%
+%   name - name assigned to the boundary condition
+%   
+%   Returns bc_path - path to the boundary condition
+%
 %   This is a wrapper for Document::createBoundaryCondition
+
     if iscell(pathArray) 
         invoke(mn, 'processcommand', ...
         sprintf('REDIM nmArray(%i)', length(pathArray)-1));
