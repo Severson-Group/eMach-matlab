6b07c120c6bb42d5c9f44b6e3f4c274ef3440977
Anvar <khamitov@wisc.edu>
2020-09-26 02:49:27 -0500

    Update PlotResults and include .svg files of plots

--- examples/LaminationProbExample/PlotResults.m
+++ examples/LaminationProbExample/PlotResults.m
@@ -1,7 +1,8 @@
-close all
-n=[1:20, 50]; lw = 3;
+close all; clc; clear;
+
+n=[1:20, 50]; lw = 2;
 % 50 laminations results:
-% M19: 
+% M19:
 lossesTrM19_50 = 0.0096; peakBTrM19_50 = 0.7727; 
 lossesTHM19_50 = 0.0316; peakBTHM19_50 = 0.4158;
 % Hiperco: 
@@ -29,13 +30,25 @@ end
 
 figure
 plot(n,[IronOhmicLossesTHM19,lossesTHM19_50],n,[IronOhmicLossesTrM19,lossesTrM19_50],'Linewidth',lw)
-xlabel('Number of laminations'); ylabel('Iron ohmic losses (W)')
-legend('Time-harmonic','Transient')
+set(gca, 'FontName', 'TimesNewRoman','TickLabelInterpreter', 'latex');  
+xlabel('Number of laminations','Interpreter','latex');
+ylabel('Iron ohmic losses (W)','Interpreter','latex')
+h1 = legend('Time-harmonic','Transient');
+set (h1,'FontName','TimesNewRoman','Interpreter', 'latex');
+title('M-19','Interpreter','latex')
+grid on
+saveas(gcf,[pwd '\PlotResults\Plot1'],'svg')
 
 figure
 plot(n,[ByStM19,1],n,[ByTHM19,peakBTHM19_50],n,[ByTrM19,peakBTrM19_50],'Linewidth',lw)
-xlabel('Number of laminations'); ylabel('|By| (T)')
-legend('Static','Time-harmonic','Transient')
+set(gca, 'FontName', 'TimesNewRoman','TickLabelInterpreter', 'latex');  
+xlabel('Number of laminations','Interpreter','latex');
+ylabel('Peak $|B_y|$ (T)','Interpreter','latex')
+h1 = legend('Static','Time-harmonic','Transient');
+set (h1,'FontName','TimesNewRoman','Interpreter', 'latex');
+title('M-19','Interpreter','latex')
+grid on
+saveas(gcf,[pwd '\PlotResults\Plot2'],'svg')
 
 %% Hiperco
 
@@ -59,33 +72,46 @@ end
 
 figure
 plot(n,[IronOhmicLossesTHHiperco,lossesTHHip_50],n,[IronOhmicLossesTrHiperco,lossesTrHip_50],'Linewidth',lw)
-xlabel('Number of laminations'); ylabel('Iron ohmic losses (W)')
-legend('Time-harmonic','Transient')
+set(gca, 'FontName', 'TimesNewRoman','TickLabelInterpreter', 'latex');  
+xlabel('Number of laminations','Interpreter','latex');
+ylabel('Iron ohmic losses (W)','Interpreter','latex')
+h1 = legend('Time-harmonic','Transient');
+set (h1,'FontName','TimesNewRoman','Interpreter', 'latex');
 ylim([0 0.4])
+title('Hiperco','Interpreter','latex')
+grid on
+saveas(gcf,[pwd '\PlotResults\Plot3'],'svg')
 
 figure
 plot(n,[ByStHiperco,1],n,[ByTHHiperco,peakBTHHip_50],n,[ByTrHiperco,peakBTrHip_50],'Linewidth',lw)
-xlabel('Number of laminations'); ylabel('|By| (T)')
-legend('Static','Time-harmonic','Transient')
+set(gca, 'FontName', 'TimesNewRoman','TickLabelInterpreter', 'latex');
+xlabel('Number of laminations','Interpreter','latex');
+ylabel('Peak $|B_y|$ (T)','Interpreter','latex')
+h1 = legend('Static','Time-harmonic','Transient');
+set (h1,'FontName','TimesNewRoman','Interpreter', 'latex');
+title('Hiperco','Interpreter','latex')
+grid on
+saveas(gcf,[pwd '\PlotResults\Plot4'],'svg')
 
 %% Both materials
 
 figure
-plot(n,[IronOhmicLossesTrM19,lossesTrM19_50],n,[IronOhmicLossesTrHiperco,lossesTrHip_50],'Linewidth',1.5)
-xlabel('Number of laminations'); ylabel('Iron ohmic losses (W)')
-legend('M-19, transient','Hiperco, transient')
+plot(n,[IronOhmicLossesTrM19,lossesTrM19_50],n,[IronOhmicLossesTrHiperco,lossesTrHip_50],'Linewidth',lw)
+set(gca, 'FontName', 'TimesNewRoman','TickLabelInterpreter', 'latex');
+xlabel('Number of laminations','Interpreter','latex');
+ylabel('Iron ohmic losses (W)','Interpreter','latex')
+h1 = legend('M-19, transient','Hiperco, transient');
+set (h1,'FontName','TimesNewRoman','Interpreter', 'latex');
 % ylim([0 0.4])
+grid on
+saveas(gcf,[pwd '\PlotResults\Plot5'],'svg')
 
 figure
 plot(n,[ByTrM19,peakBTrM19_50],n,[ByTrHiperco,peakBTrHip_50],'Linewidth',lw)
-xlabel('Number of laminations'); ylabel('|By| (T)')
-legend('M-19, transient','Hiperco, transient')
-
-%% Iron conductivity = 0
-k = ones(length(n),1); ByStCondZero = 1.05*k; ByTHCondZero = 0.71*k;
-ByTrCondZero = 1.05*k;
-figure
-plot(n,ByStCondZero,n,ByTHCondZero,n,ByTrCondZero,'Linewidth',lw)
-legend('Static','Time-harmonic','Transient')
-xlabel('Number of laminations'); ylabel('|By| (T)')
-ylim([0 1.2])
+set(gca, 'FontName', 'TimesNewRoman','TickLabelInterpreter', 'latex');
+xlabel('Number of laminations','Interpreter','latex');
+ylabel('Peak $|B_y|$ (T)','Interpreter','latex')
+h1 = legend('M-19, transient','Hiperco, transient');
+set (h1,'FontName','TimesNewRoman','Interpreter', 'latex');
+grid on
+saveas(gcf,[pwd '\PlotResults\Plot6'],'svg')
