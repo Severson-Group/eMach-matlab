2bc66ea679a90835355c8f7c66851c4425272a0d
Bharat Ramadas <70265814+Bharat-Ramadas@users.noreply.github.com>
2021-05-11 00:32:28 +0530

    Update example_arc_cross.py

--- python/examples/example_arc_cross.py
+++ python/examples/example_arc_cross.py
@@ -10,13 +10,13 @@ x = mo.DimMillimeter(4)
 y = mo.DimMillimeter(80)
 z = mo.DimDegree(40)
 
-# create hollowcylinder crossection object
+# create arc crossection object
 arc1 = mo.CrossSectArc(name = 'arc1',location = mo.Location2D(),\
                        dim_d_a = x, dim_r_o = y, dim_alpha = z)
 
 
 
-# create clone of hollowcylinder crossection
+# create another arc crossection
 arc2 = mo.CrossSectArc(name = 'arc2',location = mo.Location2D(),\
                         dim_d_a = x*2, dim_r_o = y/2, dim_alpha = z*3)
 
@@ -24,11 +24,11 @@ arc2 = mo.CrossSectArc(name = 'arc2',location = mo.Location2D(),\
 toolMn = mn.MagNet(visible=True)
 toolMn.open()
 
-# draw hollowcylinders
+# draw arcs
 c1 = arc1.draw(toolMn)
 c2 = arc2.draw(toolMn)
 
 toolMn.view_all()
-# select inner coordinate of a hollow cylinder
+# select inner coordinate of an arc
 toolMn.prepare_section(c2)
 
